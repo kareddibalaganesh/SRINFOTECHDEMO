91f4f05ebb0b6b3ed752f6f8fbc92a14d137a1e5
kareddibalaganesh <kareddibalaganesh@gmail.com>
2025-08-04 18:22:10 +0530

    i have added hello world .java class

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+
+public class SRINFOTECH {
+  public static void main(String[] args) {
+    System.out.println("SRIONFOCTEH!");
+  }
+}
